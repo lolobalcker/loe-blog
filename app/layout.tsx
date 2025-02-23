aad18e95557765b54c132e2ecbc46f505bd258f7
loe <lolobalcker@gmail.com>
2025-02-23 15:44:53 -0300

    updating...

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Link from "next/link";
-import { Linkedin, Github, Home} from 'lucide-react';
+import { Home } from 'lucide-react';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
